75253012e529f2ee8a6e9102bef7d18bb8c20c72
Andrey Zashchepin <razlivnoy@icloud.com>
2025-01-16 00:00:02 +0500

    дописаны методы класса DBManager

--- src/db_create.py
+++ src/db_create.py
@@ -7,30 +7,26 @@ load_dotenv()
 
 
 class DataBaseCreate:
-    """ Класс для создания и заполнения базы данных """
+    """Класс для создания и заполнения базы данных"""
 
-    def __init__(self, db_new_name='hh_ru'):
-        """ Конструктор класса """
-        self.dbname: str = os.getenv('DB_NAME')
-        self.__user: str = os.getenv('DB_USER')
-        self.__password: str = os.getenv('DB_PASSWORD')
-        self.__host: str = os.getenv('DB_HOST')
-        self.__port: str = os.getenv('DB_PORT')
+    def __init__(self, db_new_name="hh_ru"):
+        """Конструктор класса"""
+        self.dbname: str = os.getenv("DB_NAME")
+        self.__user: str = os.getenv("DB_USER")
+        self.__password: str = os.getenv("DB_PASSWORD")
+        self.__host: str = os.getenv("DB_HOST")
+        self.__port: str = os.getenv("DB_PORT")
         self.db_new_name: str = db_new_name
 
     def create_execute(self, query: str, params: tuple = None):
-        """ Метод для создания подключения к PostgreSQL и отправке запросов """
+        """Метод для создания подключения к PostgreSQL и отправке запросов"""
 
         conn = psycopg2.connect(
-            dbname=self.dbname,
-            user=self.__user,
-            password=self.__password,
-            host=self.__host,
-            port=self.__port
+            dbname=self.dbname, user=self.__user, password=self.__password, host=self.__host, port=self.__port
         )
         conn.autocommit = True
         try:
-            if 'select'.upper() in query.upper():
+            if "select".upper() in query.upper():
                 with conn.cursor() as cursor:
                     cursor.execute(query, params)
                     name_column = [d[0] for d in cursor.description]
@@ -39,28 +35,28 @@ class DataBaseCreate:
             else:
                 with conn.cursor() as cursor:
                     result = cursor.execute(query, params)
-                    print(f'Успешное выполнение запроса: {query}')
+                    print(f"Успешное выполнение запроса: {query}")
         except psycopg2.Error as e:
-            print(f'Ошибка выполнения запроса: {e}')
+            print(f"Ошибка выполнения запроса: {e}")
         finally:
             conn.close()
             return result
 
     def delete_database(self):
-        """ Удаление указанной базы данных, если она существует """
-        query = f'DROP DATABASE IF EXISTS {self.db_new_name}'
+        """Удаление указанной базы данных, если она существует"""
+        query = f"DROP DATABASE IF EXISTS {self.db_new_name}"
         self.create_execute(query)
 
     def create_database(self):
-        """ Метод для создания новой базы данных """
+        """Метод для создания новой базы данных"""
         self.delete_database()
-        query = f'CREATE DATABASE {self.db_new_name}'
+        query = f"CREATE DATABASE {self.db_new_name}"
         self.create_execute(query)
 
     def create_table_employers(self):
-        """ Создание таблицы с работодателями """
+        """Создание таблицы с работодателями"""
         self.dbname = self.db_new_name
-        query = '''CREATE TABLE IF NOT EXISTS employers
+        query = """CREATE TABLE IF NOT EXISTS employers
                     (
                         employer_id varchar PRIMARY KEY,
                         name varchar(50) NOT NULL,
@@ -68,13 +64,13 @@ class DataBaseCreate:
                         open_vacancies int,
                         site_url varchar,
                         description text
-                    )'''
+                    )"""
         self.create_execute(query)
 
     def create_table_vacancies(self):
-        """ Создание таблицы с работодателями """
+        """Создание таблицы с работодателями"""
         self.dbname = self.db_new_name
-        query = '''CREATE TABLE IF NOT EXISTS vacancies
+        query = """CREATE TABLE IF NOT EXISTS vacancies
                     (
                         vacancy_id serial PRIMARY KEY,
                         name varchar(100) NOT NULL,
@@ -83,13 +79,13 @@ class DataBaseCreate:
                         salary_from int,
                         salary_to int,
                         vacancy_url varchar
-                    )'''
+                    )"""
         self.create_execute(query)
 
     def fill_table_from_list(self, table_name: str, data_list: list[dict]):
-        """ Заполнение таблицы работодателей данными из списка словарей """
+        """Заполнение таблицы работодателей данными из списка словарей"""
         for data in data_list:
-            columns = ', '.join(data.keys())
-            values = ', '.join(['%s'] * len(data))
-            query = f'INSERT INTO {table_name} ({columns}) VALUES({values})'
+            columns = ", ".join(data.keys())
+            values = ", ".join(["%s"] * len(data))
+            query = f"INSERT INTO {table_name} ({columns}) VALUES({values})"
             self.create_execute(query, tuple(data.values()))
--- src/db_manager.py
+++ src/db_manager.py
@@ -4,27 +4,58 @@ from src.db_create import DataBaseCreate
 
 
 class DBManager(DataBaseCreate):
-    """ Класс для подключения к БД PostgreSQL и получения выборок """
+    """Класс для подключения к БД PostgreSQL и получения выборок"""
 
-    def __init__(self, db_new_name='hh_ru'):
-        """ Конструктор класса DBManager """
+    def __init__(self, db_new_name="hh_ru"):
+        """Конструктор класса DBManager"""
         super().__init__(self)
         self.dbname = db_new_name
 
     @staticmethod
     def tuple_in_table(select_tuple: list[tuple]):
-        """ Преобразование списка кортежей в табличку, для вывода в консоль """
+        """Преобразование списка кортежей в табличку, для вывода в консоль"""
         t = Texttable()
         t.add_rows(select_tuple)
         print(t.draw())
 
     def get_companies_and_vacancies_count(self):
-        """ Получаем список всех компаний и количество вакансий у каждой компании """
-        query = """SELECT employer_id, name, open_vacancies FROM employers
+        """Получаем список всех компаний и количество вакансий у каждой компании"""
+        query = """SELECT employer_id, name AS company, open_vacancies FROM employers
                     ORDER BY name"""
-        return self.tuple_in_table(self.create_execute(query))
+        result = self.create_execute(query)
+        return self.tuple_in_table(result)
 
+    def get_all_vacancies(self):
+        """Получаем список всех вакансий с указанием названия компании,
+        названия вакансии, зарплаты и ссылки на вакансию"""
+        query = """SELECT e.name AS company, v.name AS vacancy, v.salary_from, v.salary_to, v.vacancy_url
+                            FROM vacancies AS v
+                            INNER JOIN employers AS e USING (employer_id)"""
+        result = self.create_execute(query)
+        return self.tuple_in_table(result)
 
-if __name__ == "__main__":
-    dbm = DBManager()
-    dbm.get_companies_and_vacancies_count()
+    def get_avg_salary(self):
+        """Получаем среднюю зарплату по вакансиям"""
+        query = "SELECT AVG(salary_from) AS salary_avg FROM vacancies"
+        result = self.create_execute(query)
+        return self.tuple_in_table(result)
+
+    def get_vacancies_with_higher_salary(self):
+        """Получаем список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
+        query = """SELECT e.name AS company, v.name AS vacancy, v.salary_from, v.salary_to, v.vacancy_url
+                    FROM vacancies AS v
+                    INNER JOIN employers AS e USING (employer_id)
+                    WHERE salary_from > (SELECT AVG(salary_from) FROM vacancies)
+                    ORDER BY salary_from DESC"""
+        result = self.create_execute(query)
+        return self.tuple_in_table(result)
+
+    def get_vacancies_with_keyword(self, word: str):
+        """Получаем список всех вакансий, в названии которых содержатся переданные в метод слова"""
+        query = f"""SELECT e.name AS company, v.name AS vacancy, v.salary_from, v.salary_to, v.vacancy_url
+                    FROM vacancies AS v
+                    INNER JOIN employers AS e USING (employer_id)
+                    WHERE v.name LIKE '%{word}%'
+                    ORDER BY salary_from DESC NULLS LAST"""
+        result = self.create_execute(query)
+        return self.tuple_in_table(result)
